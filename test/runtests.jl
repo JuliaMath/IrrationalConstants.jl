c7f6a5598e4231dd15625f49fe9a5157c8fb4c73
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-06-15 21:38:20 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,40 @@
 using IrrationalConstants
 using Test
 
-@testset "IrrationalConstants.jl" begin
-    # Write your tests here.
+@testset "k*pi" begin
+  @test isapprox(2*pi, twoπ)
+  @test isapprox(4*pi, fourπ)
+  @test isapprox(pi/2, halfπ)
+  @test isapprox(pi/4, quartπ)
 end
+
+@testset "k/pi" begin
+  @test isapprox(1/pi, invπ)
+  @test isapprox(2/pi, twoinvπ)
+  @test isapprox(4/pi, fourinvπ)
+end
+
+@testset "1/(k*pi)" begin  
+  @test isapprox(1/(2pi), inv2π)
+  @test isapprox(1/(4pi), inv4π)
+end
+
+@testset "sqrt" begin
+  @test isapprox(sqrt(2), sqrt2)
+  @test isapprox(sqrt(3), sqrt3)
+  @test isapprox(sqrt(pi), sqrtπ)
+  @test isapprox(sqrt(2pi), sqrt2π)
+  @test isapprox(sqrt(4pi), sqrt4π)
+  @test isapprox(sqrt(pi/2), sqrthalfπ)
+  @test isapprox(sqrt(1/2), invsqrt2)
+  @test isapprox(sqrt(1/(2pi)), invsqrt2π)
+end
+
+@testset "log" begin
+  @test isapprox(log(1/2), loghalf)
+  @test isapprox(log(2), logtwo)
+  @test isapprox(log(pi), logπ)
+  @test isapprox(log(2pi), log2π)
+  @test isapprox(log(4pi), log4π)
+end
+
