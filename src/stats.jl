108addb7ac062bb7d11f1e49559f004ce776b6f8
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-08-11 22:03:51 +0100

    added logten, twoinvsqrtπ and halfsqrtπ

--- src/stats.jl
+++ src/stats.jl
@@ -1,15 +1,17 @@
 # mathematical constants related to statistics
 
-@irrational twoπ   6.2831853071795864769 2 * big(π)
-@irrational fourπ  12.566370614359172954 4 * big(π)
-@irrational halfπ  1.5707963267948966192 big(π) / 2
-@irrational quartπ 0.7853981633974483096 big(π) / 4
+@irrational twoπ      6.2831853071795864769 2 * big(π)
+@irrational fourπ     12.566370614359172954 4 * big(π)
+@irrational halfπ     1.5707963267948966192 big(π) / 2
+@irrational quartπ    0.7853981633974483096 big(π) / 4
+@irrational halfsqrtπ 0.886226925452758 sqrt(big(π)) / 2
 
-@irrational invπ     0.31830988618379067154 inv(big(π))
-@irrational twoinvπ  0.63661977236758134308 2 / big(π)
-@irrational fourinvπ 1.27323954473516268615 4 / big(π)
-@irrational inv2π    0.159154943091895335769 inv(2 * big(π))
-@irrational inv4π    0.079577471545947667884 inv(4 * big(π))
+@irrational invπ        0.31830988618379067154 inv(big(π))
+@irrational twoinvsqrtπ 1.1283791670955126 2 / sqrt(big(π))
+@irrational twoinvπ     0.63661977236758134308 2 / big(π)
+@irrational fourinvπ    1.27323954473516268615 4 / big(π)
+@irrational inv2π       0.159154943091895335769 inv(2 * big(π))
+@irrational inv4π       0.079577471545947667884 inv(4 * big(π))
 
 @irrational sqrt2     1.4142135623730950488 sqrt(big(2))
 @irrational sqrt3     1.7320508075688772935 sqrt(big(3))
@@ -22,7 +24,8 @@
 @irrational invsqrt2π 0.3989422804014326779 inv(sqrt(2 * big(π)))
 
 @irrational loghalf -0.6931471805599453094 log(inv(big(2)))
-@irrational logtwo 0.6931471805599453094 log(big(2))
-@irrational logπ   1.1447298858494001741 log(big(π))
-@irrational log2π  1.8378770664093454836 log(2 * big(π))
-@irrational log4π  2.5310242469692907930 log(4 * big(π))
+@irrational logtwo  0.6931471805599453094 log(big(2))
+@irrational logten  2.302585092994046 log(big(10))
+@irrational logπ    1.1447298858494001741 log(big(π))
+@irrational log2π   1.8378770664093454836 log(2 * big(π))
+@irrational log4π   2.5310242469692907930 log(4 * big(π))
